c20714f864826083a661fb6a0d7f55e70bbf7993
Rohullah <rohullahahmadi531@gmail.com>
2026-05-12 21:41:32 +0430

    s

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Alert = () => {
   return (
-    <div className="fixed top-10 m-auto p-4 flex items-center">
-      <h1>form submited successfully</h1>
-    </div>
+  
   );
 };
 
